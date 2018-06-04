fdfcc09b24d58db7e425d22d419680508865634b
mushonnifar <liesmuson@gmail.com>
2018-06-04 08:26:07 +0700

    perbaikan seeder user has role

--- database/seeds/UserhasroleTableSeeder.php
+++ database/seeds/UserhasroleTableSeeder.php
@@ -1,22 +1,20 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
-class UserhasroleTableSeeder extends Seeder {
-
+class UserhasroleTableSeeder extends Seeder
+{
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run() {
-        //
+    public function run()
+    {
         app('db')->table('std_userhasrole')->insert([
             'id' => 1,
             'user_id' => 1,
             'role_id' => 1
         ]);
     }
-
 }
